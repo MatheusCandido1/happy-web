c72fa0e9a27834c3c9779da35aaf2cf4b41bb572
Matheus Cândido <matheus9bh@hotmail.com>
2020-10-22 20:43:33 -0300

    add-orphanage-page

--- src/screens/OrphanagesMap.tsx
+++ src/screens/OrphanagesMap.tsx
@@ -1,9 +1,38 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
+import { FiPlus } from 'react-icons/fi'
+
+import MapMarker from '../assets/images/map-marker.svg';
+
+import '../styles/screens/orphanages.css';
 
 function OrphanagesMap() {
   return (
-    <h1>Maps</h1>
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={MapMarker} alt="Happy" />
+
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando a sua visita :)</p>
+        </header>
+
+        <footer>
+          <strong>Belo Horizonte</strong>
+          <span>Minas Gerais</span>
+        </footer>
+      </aside>
+
+      <div>
+
+      </div>
+
+      <Link to="" className="create-orphanage">
+        <FiPlus size={32} color="#FFF" />
+      </Link>
+    </div>
   );
 }
 
