bd2b7df83f997e98a43069b5295625810ffae4ec
Matheus Cândido <matheus9bh@hotmail.com>
2020-10-28 18:43:58 -0300

    add-create-orphanages

--- src/screens/CreateOrphanage.tsx
+++ src/screens/CreateOrphanage.tsx
@@ -3,13 +3,20 @@ import { Map, Marker, TileLayer } from 'react-leaflet';
 import { LeafletMouseEvent } from 'leaflet'
 import { FiPlus } from "react-icons/fi";
 
+import { useHistory } from "react-router-dom";
+
 import Sidebar from '../components/Sidebar'
 
 import '../styles/screens/create-orphanage.css';
 
+
 import mapIcon from "../utils/mapMarker";
 
+import api from "../services/api";
+
 export default function CreateOrphanage() {
+  const history = useHistory();
+
   const [position, setPosition] = useState({ latitude: 0, longitude: 0});
   const [name, setName] = useState('');
   const [about, setAbout] = useState('');
@@ -29,9 +36,28 @@ export default function CreateOrphanage() {
     });
   }
 
-  function handleSubmit(event: FormEvent) {
+  async function handleSubmit(event: FormEvent) {
     event.preventDefault();
     const { latitude, longitude } = position;
+
+    const data = new FormData();
+
+    data.append('name', name);
+    data.append('about', about);
+    data.append('latitude', String(latitude));
+    data.append('longitude', String(longitude));
+    data.append('instructions', instructions);
+    data.append('opening_hours', opening_hours);
+    data.append('open_on_weekends', String(open_on_weekends));
+
+    images.forEach(image => {
+      data.append('images', image);
+    });
+
+    await api.post('orphanages', data);
+    alert('Orphanage created!');
+
+    history.push('/app');
   }
 
   function handleSelectImages(event: ChangeEvent<HTMLInputElement>) {
